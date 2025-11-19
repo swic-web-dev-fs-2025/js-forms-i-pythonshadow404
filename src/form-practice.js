fbce9ac9a99dce9c4d3ac3a0ac96eb3faa9769a8
pythonshadow404 <pythonshadow404@gmail.com>
2025-11-19 15:32:56 -0600

    Add (try it 3) function component to display data in the browser

--- src/form-practice.js
+++ src/form-practice.js
@@ -1,24 +1,22 @@
 import "./style.css";
 
-function Results(data) {
-  console.info(Object.entries(data));
-
-  return `<ul class="space-y-1 text-sm">
-        ${Object.entries(data)
-          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
-          .join("")}
-      </ul>
-        `;
-}
-
+const result = document.querySelector("#result");
 const form = document.querySelector("form");
 
-// Same pattern as 'clicks', but for 'submit' events.
 form.addEventListener("submit", (event) => {
-  // No, browser! WE will handle this. Stop your default behavior.
   event.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
 
-  console.info(data);
+  // Pass the data object to the Results function to get HTML! 🚀
+  result.innerHTML = Results(data);
 });
+
+function Results(data) {
+  return `<ul class="space-y-1 text-sm">
+        ${Object.entries(data)
+          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
+          .join("")}
+    </ul>   
+    `;
+}
